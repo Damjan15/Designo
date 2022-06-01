1700eba5f111c93eba81493ef4f2967f5d4ff274
Damjan15 <stamenovdamjan20@gmail.com>
2022-06-01 04:27:09 -0700

    Folders structure setup, TailwindCSS setup

---
+++ tailwind.config.js
@@ -0,0 +1,20 @@
+module.exports = {
+  content: [
+    "./src/**/*.{js,jsx,ts,tsx}",
+  ],
+  theme: {
+    extend: {
+      colors: {
+        peach: "rgb(213, 129, 107)",
+        black: "rgb(29, 28, 30)",
+        lightPeach : "rgb(255, 173, 155)",
+        darkGrey: "rgb(51, 49, 54)",
+        lightGrey: "rgb(241, 243, 245)" 
+      },
+      fontFamily: {
+        sans: ['Jost', 'sans-serif']
+      }
+    },
+  },
+  plugins: [],
+}
